97629b1ddb28847f7e46fd2142ade256544859db
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2023-06-02 16:44:26 -0500

    mongo config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
+
 import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('MONGO') private database: Db,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+
+  getTasks() {
+    const tasks = this.database.collection('tasks');
+    return tasks.find().toArray();
   }
 }
